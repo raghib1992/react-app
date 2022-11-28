fc3859319cfecc2e8074675133e9121409c7a9df
raghib1992 <raghib.npti@gmail.com>
2022-11-28 22:02:56 +0530

    fix:added code

---
+++ release.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: https://github.com/raghib1992/react-app, 
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
+}
